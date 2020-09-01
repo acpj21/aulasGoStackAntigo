3f440ef7d2247776520389d49155a98889172dc6
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2020-09-01 08:26:13 -0300

    Tratamento de exceções

--- modulo03/src/app.js
+++ modulo03/src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+// tratamento de exceções
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,12 +13,19 @@ class App {
     constructor() {
         this.server = express();
 
+        // tratamento de exceções
+        Sentry.init(sentryConfig);
+
         this.middlewares();
 
         this.routes();
+        // tratamento de exceções
+        this.exceptionHandler();
     }
 
     middlewares() {
+        // tratamento de exceções
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -23,6 +35,16 @@ class App {
 
     routes() {
         this.server.use(routes);
+        // tratamento de exceções
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+
+            return res.status(500).json(errors);
+        });
     }
 }
 
---
+++ modulo03/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+    dsn:
+        'https://3d1793897ede48e0b3d3bafc8f424d1e@o375704.ingest.sentry.io/5413249',
+};
