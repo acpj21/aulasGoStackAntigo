8857d93566bb836fa45e601c0fcc48dc6fc22cb5
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-10-05 16:28:58 -0300

    Configurando StatusBar

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/Reactotronconfig';
 
@@ -6,6 +7,9 @@ import Routes from './routes';
 
 export default function App(){
     return (
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
         <Routes />
+      </>
       );
 }
