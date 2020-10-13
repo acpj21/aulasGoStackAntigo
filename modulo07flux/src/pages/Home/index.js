f95c4250a74ce94c1fec61dd538a7976979221ac
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-10-13 07:40:45 -0300

    Buscando produtos da API

--- modulo07flux/src/pages/Home/index.js
+++ modulo07flux/src/pages/Home/index.js
@@ -1,86 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home(){
-    return (
-        <ProductList>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1602047472&ims=326x" 
-                alt="Tênis" />
-                <strong> Tênis muito legal</strong>
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-                <span>R$ 129,90</span>
+    async componentDidMount(){
+        const response = await api.get('products');
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1602047472&ims=326x" 
-                alt="Tênis" />
-                <strong> Tênis muito legal</strong>
+        this.setState({ products: data });
+    }
 
-                <span>R$ 129,90</span>
+    render (){
+        const { products } = this.state;
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1602047472&ims=326x" 
-                alt="Tênis" />
-                <strong> Tênis muito legal</strong>
+        return (
+            <ProductList>
+                { products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} 
+                        alt={product.title}  />
+                        <strong> {product.title} </strong>
 
-                <span>R$ 129,90</span>
+                        <span>{product.priceFormatted} </span>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                            </div>
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1602047472&ims=326x" 
-                alt="Tênis" />
-                <strong> Tênis muito legal</strong>
-
-                <span>R$ 129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1602047472&ims=326x" 
-                alt="Tênis" />
-                <strong> Tênis muito legal</strong>
-
-                <span>R$ 129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
