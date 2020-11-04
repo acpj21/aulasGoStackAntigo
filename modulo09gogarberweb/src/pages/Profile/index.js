38abc31a7faa0109008318a8b92c67d31c1a6a4a
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-11-04 14:37:22 -0300

    Logout da Aplicação

--- modulo09gogarberweb/src/pages/Profile/index.js
+++ modulo09gogarberweb/src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -17,7 +18,12 @@ export default function Profile() {
     dispatch(updateProfileRequest(data));
   }
 
-  return <Container>
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+  <Container>
     <Form initialData={profile} onSubmit={handleSubmit}>
     <AvatarInput name="avatar_id" />
 
@@ -41,6 +47,9 @@ export default function Profile() {
       <button type="submit">Atualizar perfil</button>
     </Form>
 
-    <button type="submit">Sair do Gobarber</button>
+    <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
   </Container>
+  );
 }
