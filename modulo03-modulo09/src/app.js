3f748c3d9c8efdb3df3b0abb88906302113b70a1
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-11-05 09:44:35 -0300

    Outros ajustes

--- modulo03-modulo09/src/app.js
+++ modulo03-modulo09/src/app.js
@@ -5,55 +5,50 @@ import path from 'path';
 import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
-// tratamento de exceções
 import 'express-async-errors';
+
 import routes from './routes';
 import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
-    constructor() {
-        this.server = express();
+  constructor() {
+    this.server = express();
 
-        // tratamento de exceções
-        Sentry.init(sentryConfig);
+    Sentry.init(sentryConfig);
 
-        this.middlewares();
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
 
-        this.routes();
-        // tratamento de exceções
-        this.exceptionHandler();
-    }
+  middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
 
-    middlewares() {
-        // tratamento de exceções
-        this.server.use(Sentry.Handlers.requestHandler());
-        this.server.use(cors());
-        this.server.use(express.json());
-        this.server.use(
-            '/files',
-            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
-        );
-    }
+  routes() {
+    this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
 
-    routes() {
-        this.server.use(routes);
-        // tratamento de exceções
-        this.server.use(Sentry.Handlers.errorHandler());
-    }
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
 
-    exceptionHandler() {
-        this.server.use(async (err, req, res, next) => {
-            if (process.env.NODE_ENV) {
-                const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
 
-                return res.status(500).json(errors);
-            }
-
-            return res.status(500).json({ error: 'Internal server error' });
-        });
-    }
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+  }
 }
 
 export default new App().server;
