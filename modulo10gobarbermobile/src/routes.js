08fff36c297cdee6eec9acdd5ac3501377ac55b2
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-11-12 15:43:46 -0300

    Estilização das rotas

--- modulo10gobarbermobile/src/routes.js
+++ modulo10gobarbermobile/src/routes.js
@@ -1,28 +1,42 @@
 import {
   createAppContainer,
   createSwitchNavigator,
-  createBottomTabNavigator
+  createBottomTabNavigator,
 } from 'react-navigation';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
-export default (signedIn = false) =>
-createAppContainer(
-  createSwitchNavigator(
-    {
-      Sign: createSwitchNavigator({
-        SignIn,
-        SignUp,
-      }),
-      App: createBottomTabNavigator({
-        Dashboard,
-      }),
-  },
-  {
-    initialRouteName: signed ? 'App' : 'Sign'
-  }
-  )
-);
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
