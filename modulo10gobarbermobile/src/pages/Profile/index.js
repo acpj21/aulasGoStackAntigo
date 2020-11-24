e070902f9f55d19d543a004f944a507ef74f6078
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-11-24 10:22:07 -0300

    Logout

--- modulo10gobarbermobile/src/pages/Profile/index.js
+++ modulo10gobarbermobile/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import {
@@ -12,6 +13,7 @@ import {
   Form,
   FormInput,
   SubmitButton,
+  LogoutButton,
 } from './styles';
 
 export default function Profile() {
@@ -43,6 +45,10 @@ export default function Profile() {
         confirmPassword,
       })
     );
+  }
+
+  function handleLogout(){
+    dispatch(signOut());
   }
 
   return (
@@ -111,6 +117,7 @@ export default function Profile() {
           />
 
           <SubmitButton onPress={handleSubmit}>Atualizar Perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair do Gobarber</LogoutButton>
         </Form>
       </Container>
     </Background>
