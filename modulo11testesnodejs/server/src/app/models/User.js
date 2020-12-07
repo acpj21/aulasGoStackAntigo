33e60cf7604d44ff23b0edde358e4a3e911d8a3b
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-12-07 15:38:58 -0300

    Gerando dados aleatórios

--- modulo11testesnodejs/server/src/app/models/User.js
+++ modulo11testesnodejs/server/src/app/models/User.js
@@ -16,8 +16,8 @@ class User extends Model {
     );
 
     this.addHook('beforeSave', async user => {
-      if (user.password){
-        User.password_hash = await bcrypt.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
 
