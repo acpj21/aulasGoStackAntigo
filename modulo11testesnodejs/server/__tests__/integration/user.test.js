22c70afe61fe73c01e6a2f45d8c563e74d7325f9
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-12-07 14:29:18 -0300

    E-mail duplicado

--- modulo11testesnodejs/server/__tests__/integration/user.test.js
+++ modulo11testesnodejs/server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
-describe('', () => {
+import truncate from '../util/truncate';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should nob be able to register with duplicated e-mail', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.body).toBe(400);
   });
 });
